fc85e81a2a5a858cf35e6377c3191c57d89f0367
mengru <4momolmr@gmail.com>
2021-02-23 18:45:31 +0800

    done half

--- my-app/src/components/Weather/area.vue
+++ my-app/src/components/Weather/area.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="areas-unit">
-    <h1>A</h1>
+  <div id="area">
+    <h1>{{areaName}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Area',
+  props: {
+    areaName: {
+      type: String,
+      default: 'name'
+    }
+  },
   data () {
     return {
       msg: 'Welcome to Area'
     }
+  },
+  mounted: function () {
+    console.log('in area', this.areaName)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#areas-unit {
-  width: 250px;
-  background-color: aquamarine;
-  min-height: 100px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  display: flex;
-  justify-content: center;
-}
+
 </style>
--- my-app/src/components/Weather/index.vue
+++ my-app/src/components/Weather/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="weather">
+  <div id="weather">
     <h1>{{ msg }}</h1>
     <div class="areas">
-      <Area />
+      <div v-for = "(item, key, index) in status.data" :key = index class="areas-unit">
+        <Area :areaName = item.locationName />
+      </div>
+
     </div>
   </div>
 </template>
@@ -14,26 +17,46 @@ export default {
   components: {
     Area
   },
-  mounted: function () {
-    console.log('hi in weahter')
-    this.axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-077?Authorization=CWB-66C1A43E-5E26-4909-8121-A12C192396ED')
-    .then((response) => {
-      console.log(response.data)
-    })
-  },
   data () {
     return {
-      msg: 'Welcome to Weather'
+      msg: 'Welcome to Weather',
+      status: {
+        data: []
+      }
     }
+  },
+  mounted: function () {
+    this.axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-077?Authorization=CWB-66C1A43E-5E26-4909-8121-A12C192396ED').then((response) => {
+      console.log(response.data.records.locations[0].location)
+      this.$set(this.status, 'data', response.data.records.locations[0].location)
+      console.log(this.status.data)
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.weather .areas {
+#weather {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+#weather .areas {
   width: 900px;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+}
+
+#weather .areas .areas-unit {
+  width: 250px;
+  background-color: aquamarine;
+  min-height: 100px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  display: flex;
+  justify-content: center;
 }
 </style>
